d334f97a0e3601d81838792308a3bae84b064a04
Ifemide <ifemiadetayo@outlook.com>
2018-08-01 16:03:56 +0100

    small changes to fetch test data

--- src/components/InfiniteScroll/actions.js
+++ src/components/InfiniteScroll/actions.js
@@ -1,44 +1,45 @@
-import _ from 'lodash';
-import { SCROLL_START, SCROLL_SUCCESS, SCOLL_FAIL } from './constants';
+import _ from "lodash";
+import { SCROLL_START, SCROLL_SUCCESS, SCOLL_FAIL } from "./constants";
 /**
  * For handling scroller items
- * @param {*} hasMore 
- * @param {*} loadMore 
- * @param {*} atBottom 
- * @param {*} pageLoaded 
+ * @param {*} hasMore
+ * @param {*} loadMore
+ * @param {*} atBottom
+ * @param {*} pageLoaded
  */
-export const handleScrollItems = (hasMore, loadMoreData) => async(dispatch, getState) => {
+export const handleScrollItems = (hasMore, loadMoreData) => async (
+  dispatch,
+  getState
+) => {
+  const {
+    scrollData: { actionTriggered, pageLoaded }
+  } = getState();
 
-    const { scrollData: { actionTriggered, pageLoaded } } = getState();
-
-    try {
-        if (actionTriggered) {
-            return;
-        }
-        if (hasMore) {
-            let currentPage = pageLoaded;
-            dispatch({ 
-                type: SCROLL_SUCCESS, 
-                payload: { actionTriggered: true } 
-            });
-
-            if (typeof loadMoreData === 'function') {
-                await loadMoreData(pageLoaded);
-                dispatch({
-                    type: SCROLL_SUCCESS,
-                    payload: { actionTriggered : false, pageLoaded: currentPage += 1 }
-                });
-            }
-        } 
-       
-    } catch (error) {
-        dispatch({ 
-            type: MF_SCOLL_FAIL, 
-            payload: { actionTriggered: false }
-        });
+  try {
+    if (actionTriggered) {
+      return;
     }
+    if (hasMore) {
+      let currentPage = pageLoaded;
+      dispatch({
+        type: SCROLL_SUCCESS,
+        payload: { actionTriggered: true }
+      });
 
+      if (typeof loadMoreData === "function") {
+        await loadMoreData(pageLoaded);
+        dispatch({
+          type: SCROLL_SUCCESS,
+          payload: { actionTriggered: false, pageLoaded: (currentPage += 1) }
+        });
+      }
+    }
+  } catch (error) {
+    dispatch({
+      type: SCOLL_FAIL,
+      payload: { actionTriggered: false }
+    });
+  }
 };
 
 export default handleScrollItems;
-
--- src/index.js
+++ src/index.js
@@ -2,13 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import configureStore from "./store";
-
+import { prodImages } from "./Utils/index";
+import { InfiniteScroll as Scroll } from "./components";
 import "./styles.css";
+
+const resultItem = prodImages(20, true);
+
+function loadmoredata() {
+  console.log("loading more data...");
+}
+
+function getItems() {
+  let result = [];
+
+  resultItem.map((item, index) => {
+    return result.push(
+      <div>
+        <h3>{item.code}</h3>
+        <img src={item.galleryImageMaps[0].medium.url} alt="" />
+      </div>
+    );
+  });
+  return result;
+}
 
 function App() {
   return (
     <div className="App">
       <p>A simple demonstration of an infinite scroll.</p>
+      <Scroll.InfiniteScroll
+        hasMore={resultItem.length !== 0}
+        initialLoad={true}
+        loadMoreData={loadmoredata()}
+      >
+        {getItems()}
+      </Scroll.InfiniteScroll>
     </div>
   );
 }
